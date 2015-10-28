4bf9d4de68e1b7d28b17761fd142998803b351cd
Oskar <i@oskarzhang.me>
2015-10-28 18:53:14 -0500

    nicer demo

--- ListViewTest/ViewController.swift
+++ ListViewTest/ViewController.swift
@@ -21,10 +21,25 @@ class ViewController: UIViewController {
         
         var listView = TagListView(frame: CGRectMake(0, self.view.frame.height - 200, self.view.frame.width, 200))
         self.view.addSubview(listView)
-        listView.backgroundColor = UIColor.blackColor()
+        listView.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1.0)
+        listView.layer.borderColor = UIColor.blackColor().CGColor
+        listView.layer.borderWidth = 0.2
         for i in 100000000...100000500
         {
-        listView.addTag("\(i)", target: self, tapAction: "tap:", longPressAction: "longPress:",backgroundColor: UIColor.whiteColor(),textColor: UIColor.blackColor())
+            let color:UIColor!
+            if i%3 == 1
+            {
+                color = UIColor(red: 231/255, green: 76/255, blue: 60/255, alpha: 1)
+            }
+            else if i%3 == 2
+            {
+                color = UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1)
+            }
+            else
+            {
+                color = UIColor(red: 39/255, green: 174/255, blue: 96/255, alpha: 1)
+            }
+        listView.addTag("\(i)", target: self, tapAction: "tap:", longPressAction: "longPress:",backgroundColor: color,textColor: UIColor.whiteColor())
         }
     }
     func tap(sender:UIGestureRecognizer)
